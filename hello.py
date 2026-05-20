a715e8bbbdeb3ccbcfaa4f2573cdb5280537eeaa
Prashast-codes <nikkamprashast@gmail.com>
2026-05-20 21:01:34 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,174 @@
+# we can see that below the lines of codes that written in the print format to print name in an output
+print("Hello World")
+print("Coffee")
+print("Snacks")
+print("Fast Food")
+print("Decipline")
+# now see variables are the jars to store data and to label it so it can be understand that
+# what kind of data is stored in it 
+
+jar = "Protein Pwoder"
+print(jar)
+name = "Jhon"
+age = 32
+print(age)
+weight = 85
+body_temperature = 35.8
+number = 5 + 7j
+print(number)
+firstname = "cena"
+lastname = "jhon"
+fullname = lastname + " " + firstname
+print(fullname)
+print(lastname)
+print(firstname)
+quote = """ A man who is injured by the life struggle has the guts to be successful. 
+    - By Me """
+print(quote)
+print(5 > 10)
+print(5 < 10)
+name = "mukesh"
+age = 22
+weight = 69.5
+print(type(name))
+print(type(age))
+print(type(weight))
+input("What is your name:")
+username = input("Pls enter your name:")
+print(username)
+print(type(username))
+username = input("Your name")
+age = input("Your age")
+print(username)
+print(age)
+print(type(username))
+print(type(age))
+name = input("Your name:")
+print(name)
+print(type(name))
+a = 5 
+b = 2
+c = a + b 
+print("Addition:", c)
+a = 10
+b = 2
+print("modulo:", a % b)
+print("Floor Division:", a // b)
+age = 16
+requiredage = 18
+print(age > requiredage)
+age = 18
+requiredage = 18
+print(age==requiredage)
+a =  5
+b = 2 
+c = a + b
+print(c)
+d = 5
+e = 12
+f = d - e
+print(f)
+g = 10
+h = 10
+i = g * h
+print(i)
+j = 20
+k = 10
+l = j / k
+print(l)
+m = 30
+n = 5
+o = m // n
+print(o)
+p = 10
+q = 3
+r = p % q
+print(r)
+s = 5
+t = 2
+u = s ** t
+print(u)
+age = 16 
+requiredage = 18
+print(age ==requiredage)
+age1 = 16 
+requiredage2 = 18
+print(age1 < requiredage2)
+age3 = 16 
+requiredage3 = 18
+print(age3 != requiredage3)
+age4 = 16
+requiredage4 = 18
+print(age4 >= requiredage4)
+age5 = 16 
+requiredage5 = 18 
+print(age5 <= requiredage5)
+d = True
+e = False
+print(d and e)
+print(d or e)
+print(not e)
+num1 = 5
+num2 = 8
+print(num1 > num2 or num1 < num2)
+name = "Alice"
+print("X" in name)
+print("A" in name)
+print("z" not in name)
+num = 5
+print(id(num))
+num5 = 6
+num7 = 6
+num8 = 7
+print(id(num5))
+print(id(num7))
+print(id(num8))
+print(num5 is num7)
+print(num5 is num8)
+name = "Liza"
+print(name)
+print(name:= "Liza")
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("Congratulation you are eligible to vote")
+else:  
+    print("Sorry you are not eligible to vote")
+marks = int(input("Enter your marks:")) 
+if marks >= 90:
+    print("Grade: Excellent")
+elif marks < 90 and marks >= 75:
+    print("Grade: First class")  
+elif marks < 75 and marks >= 40:
+    print("Grade: Pass")
+else:
+    print("Grade: Fail")
+username = input("Enter your username:")
+password = input("Enter your password:")
+if username == "John":
+  if password == "12345":
+    print("Login successful")
+  else:
+    print("Incorrect password")
+else: 
+    print("Username not found")
+light = input("Which color is the trffic light?")
+if light == "Green":
+  print("You can go")
+elif light == "Yellow":
+  print("Go slow")
+else: 
+   print("Stop")
+player1 = 90
+player2 = 40
+player3 = 70
+if player1 > player2 and player1 > player3:
+   print("Player 1 has scored the highest")
+elif player2 > player1 and player2 > player3:
+   print("PLayer 2 has scored the highest")
+else: 
+   print("Player 3 has scored the highest")
+num = 4
+if num == 3 // 2:
+    print("The number is even")
+else:
+   print("The number is odd")
